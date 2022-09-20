811418b43cc79413d175469e06c548d73b7a1ffd
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-20 17:47:46 +0530

    added fields in Song.java class

--- src/main/java/com/niit/jukebox/model/Song.java
+++ src/main/java/com/niit/jukebox/model/Song.java
@@ -6,4 +6,10 @@
 package com.niit.jukebox.model;
 
 public class Song {
+    private int songId;
+    private String songName;
+    private String genre;
+    private String artist;
+    private String album;
+    private String duration;
 }
