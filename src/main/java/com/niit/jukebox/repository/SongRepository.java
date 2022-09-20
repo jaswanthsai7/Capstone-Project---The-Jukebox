f934f37ee0b20975274190d34f0b90e94b3dd3d6
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-20 19:49:01 +0530

    created list object in SongRepository.java

--- src/main/java/com/niit/jukebox/repository/SongRepository.java
+++ src/main/java/com/niit/jukebox/repository/SongRepository.java
@@ -5,5 +5,15 @@
  */
 package com.niit.jukebox.repository;
 
+import com.niit.jukebox.model.Song;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SongRepository {
+    List<Song> songsList;
+
+    public SongRepository() {
+        songsList = new ArrayList<>();
+    }
 }
