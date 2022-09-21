56a5ebf2dac0905ac8571e1723077152313711f1
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-21 11:03:23 +0530

    completed displayPlaylist() method in PlaylistRepository

--- src/main/java/com/niit/jukebox/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jukebox/repository/PlaylistRepository.java
@@ -5,25 +5,31 @@
  */
 package com.niit.jukebox.repository;
 
-import com.niit.jukebox.model.Playlist;
 import com.niit.jukebox.model.Song;
 import com.niit.jukebox.service.DatabaseService;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaylistRepository {
     DatabaseService databaseService;
+    Connection connection;
+    List<Song> songsList;
 
     public PlaylistRepository() {
         databaseService = new DatabaseService();
+        connection = databaseService.getConnection();
+        songsList = new ArrayList<>();
     }
 
     public void createPlaylist(String playlistName) {
         // get the database connection
         databaseService.connect();
-        Connection connection = databaseService.getConnection();
+        connection = databaseService.getConnection();
         // write the query
         String createQuery = "CREATE TABLE IF NOT EXISTS `jukebox`. ? (`playlist_id` INT PRIMARY KEY AUTO_INCREMENT,`song_id` INT ,`song_name` VARCHAR(50),`genre` VARCHAR(45),`artist` VARCHAR(45),`album` VARCHAR(45),`duration` VARCHAR(20));";
         // create an object of prepared statement
@@ -46,7 +52,7 @@ public class PlaylistRepository {
     public void addSongToPlaylist(String playlistName, Song song) {
         // get the database connection
         databaseService.connect();
-        Connection connection = databaseService.getConnection();
+        connection = databaseService.getConnection();
         // write the query
         String addQuery = "INSERT INTO `jukebox`.? (`song_id`, `song_name`, `genre`, `artist`, `album`, `duration`) VALUES (?,?,?,?,?,?);";
         // create an object of prepared statement
@@ -74,7 +80,7 @@ public class PlaylistRepository {
     public void removePlaylist(String playlistName) {
         // get the database connection
         databaseService.connect();
-        Connection connection = databaseService.getConnection();
+        connection = databaseService.getConnection();
         // write the query
         String removeQuery = "DROP TABLE IF EXISTS `jukebox`. ?;";
         // create an object of prepared statement
@@ -93,7 +99,34 @@ public class PlaylistRepository {
         }
     }
 
-    public void displayPlaylist(Playlist playlistName) {
-        // to display the playlist
+    public void displayPlaylist(String playlistName) {
+        // create a list object
+        List<Song> playlistSongs = new ArrayList<>();
+        // get the database connection
+        databaseService.connect();
+        connection = databaseService.getConnection();
+        // write the query
+        String displayQuery = "SELECT * FROM `jukebox`.?;";
+        // create an object of prepared statement
+        try (PreparedStatement preparedStatement = connection.prepareStatement(displayQuery)) {
+            preparedStatement.setString(1, playlistName);
+            ResultSet resultSet = preparedStatement.executeQuery(displayQuery);
+            // create a song object
+            Song song = new Song();
+            // use the while loop to iterate over result set
+            while (resultSet.next()) {
+                // get all the values of the result set and set to song
+                song.setSongId(resultSet.getInt("song_id"));
+                song.setSongName(resultSet.getString("song_name"));
+                song.setGenre(resultSet.getString("genre"));
+                song.setArtist(resultSet.getString("artist"));
+                song.setAlbum(resultSet.getString("album"));
+                song.setDuration(resultSet.getString("duration"));
+                playlistSongs.add(song);
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
