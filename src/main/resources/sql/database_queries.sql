6475b063438c62d182fd01e93d87b4c1ec8a3771
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-21 07:07:24 +0530

    added create table in database_queries.sql

--- src/main/resources/sql/database_queries.sql
+++ src/main/resources/sql/database_queries.sql
@@ -3,3 +3,13 @@
 
 -- use the jukebox database
    USE `jukebox`;
+
+-- create a table song in database
+   CREATE TABLE IF NOT EXISTS `jukebox`.`song` (
+       `song_id` INT PRIMARY KEY,
+       `song_name` VARCHAR(50),
+       `genre` VARCHAR(45),
+       `artist` VARCHAR(45),
+       `album` VARCHAR(45),
+       `duration` VARCHAR(20)
+   );
