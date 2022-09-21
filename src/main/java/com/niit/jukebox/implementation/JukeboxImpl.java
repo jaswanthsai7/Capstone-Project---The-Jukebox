78a7ed35e8437be0ff22062fd9c5e21b520e2b31
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-21 16:16:18 +0530

    created jukeboxImpl() method in JukeboxImpl.java

--- src/main/java/com/niit/jukebox/implementation/JukeboxImpl.java
+++ src/main/java/com/niit/jukebox/implementation/JukeboxImpl.java
@@ -5,5 +5,28 @@
  */
 package com.niit.jukebox.implementation;
 
+import com.niit.jukebox.repository.SongRepository;
+
+import java.util.Scanner;
+
 public class JukeboxImpl {
+    SongRepository songRepository;
+
+    public JukeboxImpl() {
+        songRepository = new SongRepository();
+    }
+
+    public void jukeboxImpl() {
+        System.out.println("===================================================");
+        System.out.println("                  Welcome to Jukebox               ");
+        System.out.println("===================================================\n");
+        System.out.println("1.Display All Songs \n2.Play a song \n3.Create a playlist \n4.Delete Playlist \n5.Add songs to Playlist \n6.Display Playlist");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        switch (choice) {
+            case '1':
+                songRepository.displayAllSongs();
+
+        }
+    }
 }
