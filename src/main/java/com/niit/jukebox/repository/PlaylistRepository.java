1958eb2d3883ffdc3c63935350f252a7055ebad1
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-21 10:30:47 +0530

    completed addSongToPlaylist() method in PlaylistRepository

--- src/main/java/com/niit/jukebox/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jukebox/repository/PlaylistRepository.java
@@ -6,6 +6,7 @@
 package com.niit.jukebox.repository;
 
 import com.niit.jukebox.model.Playlist;
+import com.niit.jukebox.model.Song;
 import com.niit.jukebox.service.DatabaseService;
 
 import java.sql.Connection;
@@ -23,7 +24,7 @@ public class PlaylistRepository {
         databaseService.connect();
         Connection connection = databaseService.getConnection();
         // write the query
-        String createQuery = "CREATE TABLE IF NOT EXISTS `jukebox`. ? (`playlist_id` INT AUTO_INCREMENT,`song_id` INT ,`song_name` VARCHAR(50),`genre` VARCHAR(45),`artist` VARCHAR(45),`album` VARCHAR(45),`duration` VARCHAR(20));";
+        String createQuery = "CREATE TABLE IF NOT EXISTS `jukebox`. ? (`playlist_id` INT PRIMARY KEY AUTO_INCREMENT,`song_id` INT ,`song_name` VARCHAR(50),`genre` VARCHAR(45),`artist` VARCHAR(45),`album` VARCHAR(45),`duration` VARCHAR(20));";
         // create an object of prepared statement
         try (PreparedStatement preparedStatement = connection.prepareStatement(createQuery)) {
             preparedStatement.setString(1, playlistName);
@@ -41,8 +42,31 @@ public class PlaylistRepository {
 
     }
 
-    public void addSongToPlaylist(Playlist playlistName) {
-        // to add songs to playlist
+    public void addSongToPlaylist(String playlistName, Song song) {
+        databaseService.connect();
+        Connection connection = databaseService.getConnection();
+        // write the query
+        String addQuery = "INSERT INTO `jukebox`.? (`song_id`, `song_name`, `genre`, `artist`, `album`, `duration`) VALUES (?,?,?,?,?,?);";
+        // create an object of prepared statement
+        try (PreparedStatement preparedStatement = connection.prepareStatement(addQuery)) {
+            preparedStatement.setString(1, playlistName);
+            preparedStatement.setInt(2, song.getSongId());
+            preparedStatement.setString(3, song.getSongName());
+            preparedStatement.setString(4, song.getGenre());
+            preparedStatement.setString(5, song.getArtist());
+            preparedStatement.setString(6, song.getAlbum());
+            preparedStatement.setString(7, song.getDuration());
+            // execute the query
+            int executeUpdate = preparedStatement.executeUpdate();
+            if (executeUpdate > 0) {
+                System.out.println("Successfully added the song to playlist");
+            } else {
+                System.out.println("unable to add the song to playlist");
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void removePlaylist(Playlist playlistName) {
