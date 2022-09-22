d869941b6e670e700a49c51478045f5c7df462ba
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-09-22 07:51:52 +0530

    created InvalidSongNumberException.java

---
+++ src/main/java/com/niit/jukebox/exception/InvalidSongNumberException.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name : Jaswanth Sai
+ * Date : 9/22/2022
+ * Created With: INTELLIJ IDEA COMMUNITY EDITION
+ */
+package com.niit.jukebox.exception;
+
+public class InvalidSongNumberException extends Exception {
+    public InvalidSongNumberException(String message) {
+        super(message);
+    }
+}
